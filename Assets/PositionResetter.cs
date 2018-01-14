d9a79b096fe0bbef3319b3ec7e55327255b46078
Tonny <tonnymul0908@gmail.com>
2018-01-14 15:55:32 +0700

    first commit

---
+++ Assets/PositionResetter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof (CapsuleCollider))]
+public class PositionResetter : MonoBehaviour {
+	public Vector3 startPosition;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnCollisionEnter (Collision col){
+		if(col.gameObject.tag == "Player"){
+			col.gameObject.transform.position = startPosition;
+			col.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+		}
+	}
+}
